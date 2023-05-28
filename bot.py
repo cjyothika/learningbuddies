ebc16beed8da618dae04ec8dc36c544c70abe994
Jyothika <jyothikasivasai@gmail.com>
2023-05-28 12:45:03 +0800

    Make initial commit with base code

---
+++ bot.py
@@ -0,0 +1,27 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('6233481401:AAHrhtvZ9ivUdCS-jjiq2f6IeSusyg7ZKPI').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
+
+#     Done! Congratulations on your new bot. You will find it at t.me/learningbuddies_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+# Use this token to access the HTTP API:
+# 6233481401:AAHrhtvZ9ivUdCS-jjiq2f6IeSusyg7ZKPI
+# Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
